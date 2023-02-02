0d0b54700ee43d35c3ff6a58d906274843f04789
guilherme-jafar <gjafar14@gmail.com>
2023-02-02 20:42:48 +0000

    first commit

---
+++ src/main/java/com/tecside/appEvent/AppEventApplication.java
@@ -0,0 +1,13 @@
+package com.tecside.appEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecside/appEvent/AppEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecside.appEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

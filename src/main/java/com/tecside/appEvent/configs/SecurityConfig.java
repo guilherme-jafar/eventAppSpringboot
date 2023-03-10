4c209e52fca88824a371f887febc88970a26f28e
guilherme-jafar <gjafar14@gmail.com>
2023-03-10 21:14:37 +0000

    Fixed errors

---
+++ src/main/java/com/tecside/appEvent/configs/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.tecside.appEvent.configs;
+
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//
+////        http.csrf().disable().authorizeRequests()
+////                .antMatchers("/public/**").permitAll()
+////                .anyRequest().authenticated()
+////                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+////                .and()
+////                .addFilterBefore(new JwtFilter(), null);
+//
+//    }
+
+
+
+
+//}
